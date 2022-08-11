80566c848d4b187a923a8a01c2810818bdfae986
NikeSasha <sasha.shulga.06@gmail.com>
2022-08-11 22:50:04 +0300

    C++ is my biggest weapon

---
+++ It's_mine.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define long long long
+// NikeSasha - eolymp - codeforces
+
+int main(){
+	long a = rand()%2;
+	if(a){
+		cout << "tails\n";
+	}
+	else{
+		cout << "heads\n";
+	} 
+}
